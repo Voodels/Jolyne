b32c29dab15fc6c1ca664f6fce1161dd4e696443
Ashutoshbirje <ashutoshbirje4@gmail.com>
2026-04-19 23:51:01 +0530

    ok

--- Frontend/src/components/LoginPage/LoginPage.jsx
+++ Frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import logo from '../../assets/images/Logo1.png';
 import './LoginPage.css';
 
